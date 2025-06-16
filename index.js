e598de926eb8f95b6de777a277e575652d527cfd
kiskiscat <vasa-svistun@mail.ru>
2025-06-16 22:21:14 +0400

    Отформатирован index.js и сгенерирован новый package-lock.json

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const fs = require("fs");
-const readline = require("readline");
-const crypto = require("crypto-js");
+import { createWriteStream } from "fs";
+import { createInterface } from "readline";
+import { MD5 } from "crypto-js";
 
 const NUMBER_REGEXP = /^\d{10}$/;
 const FILE_RESULT_NAME = "result.txt";
 
 async function getUserInput(prompt) {
-  const rl = readline.createInterface({
+  const rl = createInterface({
     input: process.stdin,
     output: process.stdout,
   });
@@ -19,51 +19,55 @@ async function getUserInput(prompt) {
   });
 }
 
-function getThirdPartBorders() {
+function getThirdPart() {
   const start = BigInt("0".padStart(16, "0"));
   const end = BigInt("9".repeat(16));
 
   return { start, end };
 }
+
 function getSecondPart(number) {
   const results = number.toString();
 
   return results;
 }
+
 async function getFirstPart() {
   const input = await getUserInput("Введите часть 10 цифр: ");
 
   if (!NUMBER_REGEXP.test(input)) {
-    console.error("Ошибка: нужно ввести ровно 10 цифр.");
+    console.error("Произошла ошибка: нужно ввести ровно 10 цифр.");
     process.exit(1);
   }
 
   return input;
 }
 
-async function getMD5Hashes() {
+async function getMD5HashesFromUserInput() {
   try {
     const firstPart = await getFirstPart();
-    const outputStream = fs.createWriteStream("result.txt");
+    const outputStream = createWriteStream("result.txt");
 
     console.log("Генерация комбинаций и хешей...");
+
     const startTimer = new Date();
     let count = 0;
 
     outer: for (let i = 1; i <= 99; b++) {
       const secondPart = getSecondPart(i);
-      const { start, end } = getThirdPartBorders();
+      const { start, end } = getThirdPart();
 
       for (let f = start; f <= end; f++) {
         const thirdPart = f.toString().padStart(16, "0");
         const combination = `${firstPart}.${secondPart}.${thirdPart}`;
-        const hash = crypto.MD5(combination).toString();
+        const hash = MD5(combination).toString();
 
         outputStream.write(`${combination} -> ${hash}\n`);
-
         count += 1;
+
         const endTimer = new Date();
         const time = endTimer.getTime() - startTimer.getTime();
+
         if (time > 2000) {
           break outer;
         }
@@ -79,10 +83,6 @@ async function getMD5Hashes() {
     console.error("Произошла ошибка:", error.message);
     process.exit(1);
   }
-}
-
-async function getMD5HashesFromUserInput() {
-  getMD5Hashes();
 }
 
 getMD5HashesFromUserInput();
